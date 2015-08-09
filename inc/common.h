acac4aab775d7136d8d29d2a09a2d3d54e3aca07
alex152 <alexbiba@gmail.com>
2015-08-09 19:32:05 +0300

    removed some garbage, added option to manually specify proj_root

--- inc/common.h
+++ inc/common.h
@@ -5,8 +5,10 @@
 #define SENS_REFRESH_RATE 10000
 #define BUF_SIZE 64
 
-#ifdef ALEX_COMP
+#ifdef 	ALEX_COMP
 #define PROJ_HOME "/home/alex/Workspace/hud_proj/"
+#elif	MAN_HOME
+//	to be passed as an argument to the compiler
 #else
 #define PROJ_HOME "/home/odroid/project/"
 #endif
